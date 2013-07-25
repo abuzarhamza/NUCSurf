2f8eb32d71e908621f1dd150014471f63b0f6d37
madeinindia <abuzar.hamza@gmail.com>
2013-07-26 03:39:54 +0530

    added task list for contributor

--- NUCSurf/NucSurf.pm
+++ NUCSurf/NucSurf.pm
@@ -6,18 +6,29 @@ use Crap;
 
 our $VERSION = 1.0;
 
+#Trule          Arule              Grule      Crule     ATrule
 
 sub new  {
 
 	my ($class) = @_;
 	my $self    = {
-		_content                      => "",
-		_input_format                 => "",
-		_flag_for_all_property        => "",
-		_flag_seqence_based_parameter  => "",
+		_file_content                    => "",
+		_input_format                    => "",
+		_file_name                       => "",
+
+		_flag_for_all_property           => "",
+		_flag_seqence_based_parameter    => "",
 		_flag_ohysio_cemical_parameter   => "",
 		_flag_simulation_based_parameter => "",
-		_flag_enery_parameter
+		_flag_enery_parameter            => "",
+
+		_flag_trule                      => "",
+		_flag_arule                      => "",
+		_flag_grule                      => "",
+		_flag_crule                      => "",
+		_flag_gcrule                     => "",
+		_flag_atrule                     => "",
+		
 		
 	};
 
@@ -79,7 +90,13 @@ sub EnableSeqBasedParameter {
 =head
 =cut
 sub ListSeqBasedParameter {
-
+	
+	my ($self) = @_;
+	
+	foreach (qq/_flag_trule _flag_arule _flag_grule _flag_crule _flag_gcrule _flag_atrule/) {
+		print "$_\n";
+	}
+	
 }
 
 =head
@@ -95,6 +112,10 @@ sub EnableSimulationBasedParameter {
 		$self->{_flag_simulation_based_parameter} = 'y';
 	}
 
+	foreach (qq/_flag_trule _flag_arule _flag_grule _flag_crule _flag_gcrule _flag_atrule/) {
+		$self->{$_} = 'y';
+	}
+	
 	return $self;
 }
 
@@ -107,12 +128,29 @@ sub ListSimulationBasedParameter {
 =head
 =cut
 sub EnablePhysioChemicalParameter {
+	
 }
 
 =head
 =cut
 sub ListPhysioChemicalParamter {
-}
-1;
 
+}
+
+=head
+//to do for noor :)
+1. create a subroutine to parse the fasta file
+2. It would be nice if some existing perl module could be use for parsing
+3. the subroutine sould save the object in key so that it could be use in another sunroutine 
+for futhur processing
+4. for now commiting in master will be better
+5. _file_name or from _file_content (for webtool development) 
+
+//to do for noor 
+1. subroutine to set the file
+2. subroutine to get the file
+=cut
+
+
+1;
 __END__
